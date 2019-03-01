2d02602112b0ce10913e9731e5a0d086bed93f45
Nerea Pascual Diaz <nereapasc@gmail.com>
2019-03-01 17:19:52 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-Cocktail.new(name: 'Mint Julep')
-Cocktail.new(name: 'Whiskey Sour')
-Cocktail.new(name: 'Mojito')
+require 'open-uri'
+require 'json'
 
-Ingredient.new(name: 'lemon')
-Ingredient.new(name: 'ice')
-Ingredient.new(name: 'mint leaves')
+results = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+
+results['drinks'].each do |ingredient|
+  Ingredient.new(name: ingredient["strIngredient1"]).save
+end
+
+Cocktail.create(name: "Sex on the beach", image_url: "https://unsplash.com/photos/fXkmfjaowOw")
+Cocktail.create(name: "Mojito", image_url:"https://unsplash.com/photos/pb7oJwtNVU4")
+Cocktail.create(name: "White Russian", image_url:"https://unsplash.com/photos/btOgwJQvu28")
